772cdfa39f3a03390c46983b3399e3cd781b5d04
Jakaria Blaine <jakariablaine120@gmail.com>
2021-06-03 14:58:36 +0600

    Add logo and landing page

---
+++ app/src/main/java/com/MonkeTeam/psychotherapia/LoadingOverlay.java
@@ -0,0 +1,21 @@
+package com.MonkeTeam.psychotherapia;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LoadingOverlay#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoadingOverlay extends Fragment {
+    public LoadingOverlay()
+    {
+        super(R.layout.fragment_loading_overlay);
+    }
+}
--- app/src/main/java/com/MonkeTeam/psychotherapia/MainActivity.java
+++ app/src/main/java/com/MonkeTeam/psychotherapia/MainActivity.java
@@ -1,8 +1,11 @@
 package com.MonkeTeam.psychotherapia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null)
+        {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.overlay_frame_container_view, LoadingOverlay.class, null)
+                    .commit();
+        }
+
+        (new Handler(Looper.getMainLooper())).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                hideLoadingFragment();
+            }
+        }, 2000);
+    }
+
+    public void hideLoadingFragment()
+    {
+        Fragment loading_overlay_fragment = getSupportFragmentManager().findFragmentById(R.id.overlay_frame_container_view);
+
+        getSupportFragmentManager().beginTransaction()
+                .setCustomAnimations(0, R.anim.fade_out)
+                .hide(loading_overlay_fragment)
+                .commit();
+
     }
 }
